3f930e152532299c8ee72cab604fc6b852fcf8a6
Christopher Hernandez <cerberean661@gmail.com>
2019-08-14 09:31:28 -0700

    running into export error in app

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -6,7 +6,7 @@ import OperatorButton from "./OperatorButton.js";
 //Import your array data to from the provided data file
 import {operators} from '../../../data.js';
 
-const Operators = () => {
+const Operators = (props) => {
   // STEP 2 - add the imported data to state
 
   return (
@@ -14,7 +14,7 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {operators.map(operator => <OperatorButton operator={operator}/>)}
+       {operators.map(operator => <OperatorButton operator={operator} addOperator={props.addOperator} />)}
     </div>
   );
 };
